0cbec36686121f07e67fb4f76d2b1af763a35422
Soufiane <aabbosoufiane1@gmail.com>
2022-03-02 11:35:25 +0100

    exo5finis

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <a href="nom">nom</a>
+    <a href="/prenom">prenom</a>
+    <a href="/age">age</a>
+    
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/nom', function () {
+    return view('nom');
+})->name('nom');
+
+Route::get('/prenom', function () {
+    return view('prenom');
+});
+
+Route::get('/age', function () {
+    return view('age');
+});
+
